57c6c627eb6b7bbf6fd677279fd0d4c8a34c2797
Rein <szgzlye@yahoo.co.jp>
2018-06-05 16:39:20 +0900

    test

---
+++ GetInput.py
@@ -0,0 +1,13 @@
+
+print("Hello Python World")
+
+i = 0
+
+while i == 0:
+	getInput = input("Please input anything!:")
+
+	if getInput == str(0):
+		print("You input is num")
+		i += 1
+	else:
+		print("I don't know now")
